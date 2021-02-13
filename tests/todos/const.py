aa2a7659b846f8620f45093378ce609119886433
Kevin Chuong <kevin.chuong@mail.mcgill.ca>
2021-02-13 03:37:07 -0500

    Basic Tests for main API

---
+++ tests/todos/const.py
@@ -0,0 +1,33 @@
+TODOS_DEFAULT_JSON = {
+    "todos": [
+        {
+            "id": "2",
+            "title": "file paperwork",
+            "doneStatus": "false",
+            "description": "",
+            "tasksof": [
+                {
+                    "id": "1"
+                }
+            ]
+        },
+        {
+            "id": "1",
+            "title": "scan paperwork",
+            "doneStatus": "false",
+            "description": "",
+            "categories": [
+                {
+                    "id": "1"
+                }
+            ],
+            "tasksof": [
+                {
+                    "id": "1"
+                }
+            ]
+        }
+    ]
+}
+
+TODOS_DEFAULT_XML = "<todos><todo><doneStatus>false</doneStatus><description/><tasksof><id>1</id></tasksof><id>2</id><title>file paperwork</title></todo><todo><doneStatus>false</doneStatus><description/><tasksof><id>1</id></tasksof><id>1</id><categories><id>1</id></categories><title>scan paperwork</title></todo></todos>"
