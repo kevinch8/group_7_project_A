a910aead59aed5994161918ffdcccefbb6106a5e
Kevin Chuong <kevin.chuong@mail.mcgill.ca>
2021-02-12 21:41:45 -0500

    basic template

---
+++ tests/todos/test_todos.py
@@ -0,0 +1,18 @@
+import requests
+import subprocess
+import unittest
+import time
+class TestTodos(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.process = subprocess.Popen(['java', '-jar', '../../runTodoManagerRestAPI-1.5.5.jar'])
+
+    def test_1(self):
+
+        request = requests.get('http://localhost:4567/todos')
+        self.assertEqual(request.text, 2)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.process.terminate()
